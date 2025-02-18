7a0c5ccd3c11074256608805ea09cf0883e1f767
Max Rainer <maxrainer18@gmail.com>
2025-02-18 09:07:31 +0100

    rest

--- app/restapi.py
+++ app/restapi.py
@@ -6,3 +6,7 @@ markets = ['BTC','ETH']
 @app.route('/markets')
 def get_markets():
     return jsonify(markets)
+
+class Rest_API():
+    def __init__(self):
+        app.run(host='0.0.0.0', port=1111, debug=True)
