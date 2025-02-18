ae84dd6531a87d55d7c1c1e4bdd8ca9de60297cc
Max Rainer <maxrainer18@gmail.com>
2025-02-18 09:13:57 +0100

    rest

--- app/restapi.py
+++ app/restapi.py
@@ -1,12 +1,8 @@
-from flask import Flask, jsonify, request
+# from flask import Flask, jsonify, request
 
-app = Flask(__name__)
-markets = ['BTC','ETH']
+# app = Flask(__name__)
+# markets = ['BTC','ETH']
 
-@app.route('/markets')
-def get_markets():
-    return jsonify(markets)
-
-class Rest_API():
-    def __init__(self):
-        app.run(host='0.0.0.0', port=1111, debug=True)
+# @app.route('/markets')
+# def get_markets():
+#     return jsonify(markets)
