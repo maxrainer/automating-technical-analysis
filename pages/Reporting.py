f8991a04a549a28e38fcd59ad051fc2484757c86
Max Rainer <maxrainer18@gmail.com>
2025-02-22 21:48:33 +0100

    menu new

--- pages/Reporting.py
+++ pages/Reporting.py
@@ -6,42 +6,39 @@ from app.indicator_analysis import Indications
 from tensorflow.keras.models import load_model
 import pandas as pd
 import numpy as np
+import time
 
-gc.collect()
+exchange = 'Binance'
+market = 'USDT'
+indication = 'Predicted'
+# to be changed
+#risk='Medium'
 
-def equity_to_link(equity):
-    return "https://www.binance.com/en/trade/" + equity + "_USDC?_from=markets&type=spot"
 
-def main(app_data):
-    analysis = []
-    analysis_days = []
-    current_prices = []
-    requested_prediction_prices = []
-    changes = []
-    requested_prediction_actions = []
-    buy_prices = []
-    sell_prices = []
-    margins = []
-    binance_urls = []
+def get_data_source():
+    if st.session_state.source_file == 'Short':
+        data_source = pd.read_csv('market_data/binance_reporting_short.txt')
+    elif st.session_state.source_file == 'Medium':
+        data_source = pd.read_csv('market_data/binance_reporting.txt')
+    #all
+    else:
+        data_source = pd.read_csv('market_data/binance_reporting_long.txt')
 
-    
-    exchange = 'Binance'
-    market = 'USDT'
-    indication = 'Predicted'
-    # to be changed
-    risk='Medium'
 
-    data_source = pd.read_csv('market_data/binance_reporting.txt')
     equitys = data_source['Currency']
     if equitys.size == 0: 
         st.markdown("no Equity, exit now")
         exit
-    
-    st.sidebar.subheader('Interval:')
-    interval = st.sidebar.selectbox('', ('1 Minute', '3 Minute', '5 Minute', '15 Minute', '30 Minute', '1 Hour', '6 Hour', '12 Hour', '1 Day', '1 Week'), index = 8)
+    return equitys
 
-    st.title(f'AI Driven Reporting.')
-    st.subheader(f'Data Sourced from {exchange}.')
+def equity_to_link(equity):
+    return "https://www.binance.com/en/trade/" + equity + "_USDC?_from=markets&type=spot"
+
+def compute_model(equitys, interval):
+    analysis = []
+    analysis_days = []
+    binance_urls = []
+    coins = []
 
     _to_be_deleted = []
     _equity_percentage = 100/equitys.size
@@ -54,54 +51,94 @@ def main(app_data):
         try:
             analysis.append(Visualization(exchange, interval, equity, indication, action_model, price_model, market))
             analysis_days.append(Indications(exchange, '1 Day', equity, market))
-        except: 
+        except Exception as error: 
+#            st.markdown(type(error).__name__)
             progress_bar.progress(percentage_complete, text=f'SHOT BY HUNTER! **{equity}** is dead')
             _to_be_deleted.append(pd.Index(equitys).get_loc(equity))
         else: 
             progress_bar.progress(percentage_complete, text=f'Carrier pigeon arrived for **{equity}**')
             binance_urls.append(equity_to_link(equity))
+            coins.append(equity)
+
     progress_bar.empty()
     for d in _to_be_deleted:
         equitys = equitys.drop(d)
+    return analysis, analysis_days, binance_urls, coins
+
+def format_usd(usd):
+    if float(usd) >= 100:
+        usd = f'{float(usd):,.2f}'
+    elif float(usd) >= 10:   
+        usd = f'{float(usd):,.6f}'
+    else:
+        usd = f'{float(usd):,.8f}'
+    return usd 
+
+def get_confidence(analysis):
+    accuracy_threshold = {analysis.score_action: 75., analysis.score_price: 75.}
+    confidence = ""
+    for score, threshold in accuracy_threshold.items():
+        if float(score) >= threshold:
+            confidence = float(score)
+    return confidence
+
+def analyze(ds, analysis, analysis_days):
+    ds['current_prices'] = []
+    ds['requested_prediction_prices'] = []
+    ds['prediction_percentage'] = []
+    ds['changes'] = []
+    ds['requested_prediction_actions'] = []
+    ds['buy_prices'] = []
+    ds['sell_prices'] = []
+    ds['margins'] = []
+    ds['confidences'] = []
 
     count = 0
     for analyse in analysis: 
         current_price = float(analyse.df['Adj Close'][-1])
         requested_prediction_price = float(analysis[count].requested_prediction_price)
+        prediction_percentage = (float((requested_prediction_price/current_price)*100)-100)
         risks = {'Low': [analysis_days[count].df['S1'].values[-1], analysis_days[count].df['R1'].values[-1]], 
             'Medium': [analysis_days[count].df['S2'].values[-1], analysis_days[count].df['R2'].values[-1]],   
             'High': [analysis_days[count].df['S3'].values[-1], analysis_days[count].df['R3'].values[-1]],}
-        buy_prices.append(f'{float(risks[risk][0]):,.6f}')
-        sell_prices.append(f'{float(risks[risk][1]):,.6f}')
-        current_prices.append(f'{float(current_price):,.6f}')
-        requested_prediction_prices.append(f'{float(requested_prediction_price):,.6f}')
-        changes.append(float(analyse.df['Adj Close'].pct_change()[-1]) * 100)
-        requested_prediction_actions.append(analyse.requested_prediction_action)
-        margin = ((float(risks[risk][1]) / float(risks[risk][0])*100)-100)
-        margins.append(f'{float(margin):,.2f}')
-
+        ds['buy_prices'].append(format_usd(risks[st.session_state.risk][0]))
+        ds['sell_prices'].append(format_usd(risks[st.session_state.risk][1]))
+        confidence = get_confidence(analyse)
+        ds['confidences'].append(confidence)
+        ds['current_prices'].append(format_usd(current_price))
+        ds['requested_prediction_prices'].append(format_usd(requested_prediction_price))
+        ds['prediction_percentage'].append(f'{float(prediction_percentage):,.2f}')
+        ds['changes'].append(float(analyse.df['Adj Close'].pct_change()[-1]) * 100)
+        ds['requested_prediction_actions'].append(analyse.requested_prediction_action)
+        margin = ((float(risks[st.session_state.risk][1]) / float(risks[st.session_state.risk][0])*100)-100)
+        ds['margins'].append(f'{float(margin):,.2f}')
         count += 1
+    return ds
 
-    df1 = pd.DataFrame(
+def get_panda_dataframe(ds):
+    panda_frame = pd.DataFrame(
         {
-            "coin": binance_urls,
-            "action": requested_prediction_actions,
-            "current": current_prices,
-            "prediction": requested_prediction_prices,
-            "change": changes,
-            "buy": buy_prices,
-            "sell": sell_prices,
-            "margin": margins
-        },
+            "coin": ds['binance_urls'],
+            "action": ds['requested_prediction_actions'],
+            "current": ds['current_prices'],
+            "change": ds['changes'],
+            "prediction": ds['requested_prediction_prices'],
+            "prediction_percentage": ds['prediction_percentage'],
+            "confidence": ds['confidences'],
+            "buy": ds['buy_prices'],
+            "sell": ds['sell_prices'],
+            "margin": ds['margins']
+        }
     )
-    requested_date = analysis[0].df.index[-1]
-    st.markdown(f'**Prediction Date & Time (UTC):** {str(requested_date)}.')
+    return panda_frame
 
+@st.fragment
+def get_dataframe(pdf, ds):
     event = st.dataframe(
-        df1,
+        pdf,
         hide_index=True,
-        width=1200,
-    column_order=["coin", "action", "current", "change", "prediction", "buy", "sell", "margin"],
+        on_select="rerun",
+        selection_mode="single-row",
         column_config={
             "coin": st.column_config.LinkColumn(
                 "Coin",
@@ -109,12 +146,20 @@ def main(app_data):
                 display_text=r"https://www.binance.com/en/trade/(.*?)_USDC.*"
             ),
             "action": "Action",
-            "current": "Current($)",
+            "current": "Current $",
             "change":  st.column_config.NumberColumn(
-                    "Change",
+                    "Past",
                     format="%.2f%%"
             ),
-            "prediction": "Predicted($)",
+            "prediction": "Forecast",
+            "prediction_percentage": st.column_config.NumberColumn(
+                "Future",
+                format="%.2f%%"
+            ),
+            "confidence": st.column_config.NumberColumn(
+                "P (*)",
+                format="%.2f%%"
+            ),
             "buy": "Buy Limit",
             "sell": "Sell Limit",
             "margin": st.column_config.NumberColumn(
@@ -123,16 +168,67 @@ def main(app_data):
             )
         }
     )
+    if event.selection['rows']:
+        order_dialog(ds, event.selection['rows'][0])
+
+def execute_order(limit, amount, take_profit, stop_loss):
+    st.write("executed")
+
+@st.dialog("Execute Order")
+def order_dialog(ds, row):
+    st.markdown(f"**Trigger Limit Orders for Coin** {ds['coins'][row]} on {exchange}.")
+    st.markdown(f"**Predicted Price in {st.session_state.interval.replace(',','')} is:** ${ds['requested_prediction_prices'][row]}")
+
+    limit_buy = st.text_input("Buy Limit Order", value=float(ds['buy_prices'][row].replace(",",""),))
+    order_coin_amount = st.text_input("Amount", value=float(1))
+    st.write("Total buy price: $", float(float(limit_buy) * float(order_coin_amount)))
+    st.markdown(f'TP/SL')
+    col1, col2 = st.columns(2)
+    with col1:
+        take_profit = st.text_input("TP Limit", value=float(ds['sell_prices'][row].replace(",",""),))
+    with col2:
+        stop_loss = st.text_input("Stop Loss Offset %", value=2.0)
+    if st.button("Place Order"):
+        execute_order()
+
+@st.fragment
+def get_menu_left():
+    st.subheader('Coin List:')
+    st.session_state.source_file = st.selectbox('', ('Short', 'Medium', 'all'), index=0)
+    st.subheader('Interval:')
+    st.session_state.interval = st.selectbox('', ('1 Minute', '3 Minute', '5 Minute', '15 Minute', '30 Minute', '1 Hour', '6 Hour', '12 Hour', '1 Day', '1 Week'), index = 8)
+    st.subheader('Trading Volatility:')
+    st.session_state.risk = st.selectbox('', ('Low', 'Medium', 'High'), index = 1)
+
+def main(ds):
+    exchange = 'Binance'
+    with st.sidebar:
+        get_menu_left()
+        st.button('Start')
+
+    st.title(f'AI Driven Reporting.')
+    st.markdown(f'Data Sourced from {exchange}')
+
+    equitys = get_data_source()
+    analysis, analysis_days, ds['binance_urls'], ds['coins'] = compute_model(equitys, st.session_state.interval)
+    ds = analyze(ds, analysis, analysis_days)
+
+    requested_date = analysis[0].df.index[-1]
+    st.write(f'**Prediction Date & Time (UTC):** {str(requested_date)}')
+    st.write(f'**Time frame forecast:** {st.session_state.interval}')
+    st.write(f'(*) Probability/Confidence forecast will occur')
+
+    panda_dataframe = get_panda_dataframe(ds)
+    get_dataframe(panda_dataframe, ds)
 
 if __name__ == '__main__':
     import warnings
     import gc
     warnings.filterwarnings("ignore") 
+    ds = {}
 
     action_model = load_model("models/action_prediction_model.h5")
     price_model = load_model("models/price_prediction_model.h5")
 
-    app_data = Data_Sourcing()
     gc.collect()
-
-    main(app_data = app_data)
+    main(ds)
